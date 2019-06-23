cace8b9b7f3edb90b9ad53d1191ad91f81e1dba8
szczepaniakmichal <mssk87@gmail.com>
2019-06-23 12:06:05 +0200

    create component tweet

---
+++ src/scripts/components/tweet.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="tweet">
+        To jest mój pierwzy tweet! :)
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Tweet'
+}
+</script>
+
+<style>
+
+</style>
