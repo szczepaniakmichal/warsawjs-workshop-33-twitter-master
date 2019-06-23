404c4c045cd73362575403b354233d05959a2938
szczepaniakmichal <mssk87@gmail.com>
2019-06-23 12:05:34 +0200

    create component app

---
+++ src/scripts/components/app.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <Tweet></Tweet>
+        <Tweet></Tweet>
+        <Tweet></Tweet>
+        <Tweet></Tweet>
+        <Tweet></Tweet>
+    </div>
+</template>
+
+<script>
+import Tweet from '@/components/tweet';
+export default {
+    name: 'App',
+    components: {
+        Tweet
+    }
+}
+</script>
+
+<style>
+
+</style>
